c6db698df3684484874200b032460a129c5e84ac
lecom <lecom@localhost.localdomain>
2016-03-31 12:28:37 +0200

    Experimental compiling on Windows

---
+++ configure.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/DerelictOrg/DerelictSDL2 --branch 2.0.2
+move ./DerelictSDL2/source/derelict .
+git clone https://github.com/DerelictOrg/DerelictENet
+move ./DerelictENet/source/derelict/enet ./derelict
+git clone https://github.com/DerelictOrg/DerelictUtil
+move ./DerelictUtil/source/derelict/util ./derelict
